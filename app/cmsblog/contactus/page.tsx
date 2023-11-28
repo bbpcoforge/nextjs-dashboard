d207f78dc060d7dbf7a1a25b4852b1ce4cd82366
bbpcoforge <bipin.pandey@coforge.com>
2023-11-28 14:35:37 +0530

    revert fix

--- app/cmsblog/contactus/page.tsx
+++ app/cmsblog/contactus/page.tsx
@@ -1,12 +1,7 @@
-//import { getContactusFormFromCms } from "@/app/lib/api";
-//import ContactUs from "@/app/ui/cmsblog/contact-us";
+import { getContactusFormFromCms } from "@/app/lib/api";
+import ContactUs from "@/app/ui/cmsblog/contact-us";
 
 export default async function Page() {
-  //const formFields = await getContactusFormFromCms();
-  return (
-    <>
-      Contact Us!!!
-      {/*<ContactUs formFields={formFields} />*/}
-    </>
-  );
+  const formFields = await getContactusFormFromCms();
+  return <ContactUs formFields={formFields} />;
 }
